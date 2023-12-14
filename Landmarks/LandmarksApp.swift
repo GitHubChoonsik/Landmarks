0d06f020f100c618897dd3d19be8231670299769
GitHubChoonsik <nsy3576@gmail.com>
2023-12-14 14:50:39 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by SkyMac on 2023/12/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
